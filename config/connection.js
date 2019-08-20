15c9ec0ca5f183510aedc3c253f5d59d2e4d5f2d
Andrew <siricoa94@github.com>
2019-08-20 00:34:52 -0400

    added connection.js and orm.js, also added schema.sql and populated it with seeds.sql. Furthermore, added a .gitignore

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Adriana98!",
+    database: "burgers_db"
+  });
+};
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  connection.config.typeCast = function(field, next) {
+    if (field.type == "TINY" && field.length == 1) {
+      return field.string() == "1"; // 1 = true, 0 = false
+    }
+    return next();
+  };
+  
+  module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Bill", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Locanocnar", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES("BillTwoPointOh", false);
