7eee4fd2ef41263d2351100a85268ccc97fb14b7
Andrew <siricoa94@github.com>
2019-08-21 14:33:40 -0400

    setting up more folder and file structure. Working out pathing issues

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers(
 	id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30),
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
+
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static("public"));
+
+app.listen(PORT, function () {
+    console.log("App listening on: http://localhost:" + PORT);
+});
